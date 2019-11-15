ae867a5bb32a180c6e49fbd155033c5d83618495
Jan Werkhoven <jan.werkhoven@gmail.com>
2019-11-15 20:03:33 +1100

    Use Fish instead of Bash for deployment

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,26 @@
-#!/usr/bin/env bash
+#!/usr/local/bin/fish
 
-set -e
-set -o pipefail
+set remote "rv@server.interflux.com"
+set path "/var/www/floatplane.dev"
 
-branch=$(git rev-parse --abbrev-ref HEAD)
-revision=$(git rev-parse --short HEAD)
+set branch (git rev-parse --abbrev-ref HEAD)
+set revision (git rev-parse --short HEAD)
 
-echo "----------"
-echo "Deploying:"
-echo $branch
-echo $revision
-echo "----------"
-echo "scp install.sh deploy@server-singapore.nabu.io:/var/www/floatplane.dev"
-scp install.sh deploy@server-singapore.nabu.io:/var/www/floatplane.dev
-echo "----------"
-echo 'ssh deploy@server-singapore.nabu.io "/var/www/floatplane.dev/install.sh $branch $revision"'
-ssh deploy@server-singapore.nabu.io "/var/www/floatplane.dev/install.sh $branch $revision"
+echo ----------
+echo Deploying:
+echo Branch: $branch
+echo Revision: $revision
+echo Remote: $remote
+echo ----------
+
+switch $branch
+case production
+  echo scp remote/install.sh $remote:$path
+  scp remote/install.sh $remote:$path
+  and echo ----------
+  and echo ssh $remote "$path/install.sh $branch $revision"
+  and ssh $remote "$path/install.sh $branch $revision"
+case '*'
+    echo Aborting - Only the branch production is deployable.
+    echo ----------
+end

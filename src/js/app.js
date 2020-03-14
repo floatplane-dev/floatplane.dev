c98455853a6dbf9db3e4a119a1ffd972e0618d9d
Jan Werkhoven <jan.werkhoven@gmail.com>
2020-03-14 13:32:53 +1100

    Hide the cube

--- src/js/app.js
+++ src/js/app.js
@@ -22,38 +22,37 @@
 */
 
 document.addEventListener("DOMContentLoaded", function() {
-
   const scene = generateScene();
   const camera = generateCamera();
   const renderer = generateRenderer();
-  const cube = generateCube();
+  // const cube = generateCube();
   const light = generateLight();
   const water = generateWater(scene, light);
   const sky = generateSky(scene, renderer, water, light);
 
-	scene.add( light );
-	scene.add( water );
-  scene.add(cube);
+  scene.add(light);
+  scene.add(water);
+  // scene.add(cube);
 
-  var animate = function () {
-    requestAnimationFrame( animate );
+  var animate = function() {
+    requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    water.material.uniforms[ 'time' ].value += 0.5 / 60.0;
+    // cube.rotation.x += 0.01;
+    // cube.rotation.y += 0.01;
+    water.material.uniforms["time"].value += 0.5 / 60.0;
 
-    renderer.render( scene, camera );
+    renderer.render(scene, camera);
   };
 
-  window.addEventListener( 'resize', onWindowResize, false );
+  window.addEventListener("resize", onWindowResize, false);
 
   function onWindowResize() {
-		camera.aspect = window.innerWidth / window.innerHeight;
-		camera.updateProjectionMatrix();
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
 
-		renderer.setSize( window.innerWidth, window.innerHeight );
-	}
+    renderer.setSize(window.innerWidth, window.innerHeight);
+  }
 
-  document.getElementById('homepage').appendChild( renderer.domElement );
+  document.getElementById("homepage").appendChild(renderer.domElement);
   animate();
 });
